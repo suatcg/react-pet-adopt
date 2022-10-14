d9b0bfd6ae435d6068969a0470cac3e3dcedd706
suatcg <suat-can_24@hotmail.com>
2022-10-14 11:55:58 +0300

    code-splitted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
-      <BrowserRouter>
-        <ThemeContext.Provider value={theme}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </ThemeContext.Provider>
-      </BrowserRouter>
+      <Suspense fallback={<h2>Loading ...</h2>}>
+        <BrowserRouter>
+          <ThemeContext.Provider value={theme}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </ThemeContext.Provider>
+        </BrowserRouter>
+      </Suspense>
     </StrictMode>
   );
 };
