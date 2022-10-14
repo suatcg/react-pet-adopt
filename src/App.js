2bb147a3436c54a079987365b696cd3a14297a0a
suatcg <suat-can_24@hotmail.com>
2022-10-14 13:23:19 +0300

    server-side-rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { StrictMode, useState } from "react";
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import { Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
@@ -9,19 +9,17 @@ const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
-      <BrowserRouter>
-        <ThemeContext.Provider value={theme}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </ThemeContext.Provider>
-      </BrowserRouter>
+      <ThemeContext.Provider value={theme}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
